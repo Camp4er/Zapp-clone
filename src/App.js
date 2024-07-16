601d5970657a58bf226e5b895046c61dadbb0d41
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-07-16 14:24:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+// import './Assets/fonts.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
